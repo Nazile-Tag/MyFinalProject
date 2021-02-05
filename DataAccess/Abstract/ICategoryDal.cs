e82cb8352f03bde499b0e23527123b4f57dcd8d0
Nazile-Tag <tagnazile@gmail.com>
2021-02-05 19:29:32 +0300

    Güncellendi

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface ICategoryDal:IEntityRepository<Category>
     {
-        
+
 
     }
 }
